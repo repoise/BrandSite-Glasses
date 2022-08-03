45f51abf8e110d1d5889b73e306bb1b06727e0bc
IreneY <gognofney@gmail.com>
2022-08-03 15:45:21 +0800

    Update 2022-08-03T07:45:20.318Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,10 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+}); // header ham-icon
+
+$('.ham-icon').click(function (e) {
+  e.preventDefault();
+  $('.nav-list').toggleClass('active');
 });
 //# sourceMappingURL=all.js.map
