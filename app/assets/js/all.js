810b6819cb7f30da613941b64a02d049f3d6d934
IreneY <gognofney@gmail.com>
2022-08-03 15:46:34 +0800

    新增漢堡選單 新增常見問題RWD

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,10 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+// header ham-icon
+$('.ham-icon').click(function(e){
+  e.preventDefault();
+  $('.nav-list').toggleClass('active');
+
+})
